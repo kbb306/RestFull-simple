87e5f60ea8d60db0653730e50bfb2fc5580d70e9
kbb306 <kylerbbelshaw@gmail.com>
2026-02-13 01:27:25 -0700

    Removed unnecessary name attribute

--- app/src/main/java/com/example/restfullsimple/RestFull.kt
+++ app/src/main/java/com/example/restfullsimple/RestFull.kt
@@ -2,13 +2,10 @@ package com.example.restfullsimple
 
 
 class Percent {
-    var name : String? = null
     var threshold : Int? = null
 
 
-    constructor(name: String, threshold: Int) {
-        require(name != "" && threshold in (0..100))
-        this.name = name
+    constructor(threshold: Int) {
         this.threshold = threshold
 
     }
